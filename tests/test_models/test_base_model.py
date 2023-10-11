8826c4afb317cc76e72b3a8176ea28148c1b4e5d
Shaker Sharabi <sheko100@icloud.com>
2023-10-11 20:51:32 +0300

    Add testing for model copying

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,76 +12,76 @@ class TestBaseModel(unittest.TestCase):
     def test_id_uniquness(self):
         """Test that the instances ids are different
         """
-        obj1 = BaseModel()
-        obj2 = BaseModel()
+        model1 = BaseModel()
+        model2 = BaseModel()
 
-        self.assertNotEqual(obj1.id, obj2.id)
+        self.assertNotEqual(model1.id, model2.id)
 
     def test_model_uniquness(self):
         """Test that the instances are different entities
         """
 
-        obj1 = BaseModel()
-        obj2 = BaseModel()
+        model1 = BaseModel()
+        model2 = BaseModel()
 
-        self.assertNotEqual(obj1, obj2)
+        self.assertNotEqual(model1, model2)
 
     def test_id_type(self):
         """Test that the id attribute is a string
         """
 
-        obj1 = BaseModel()
-        self.assertEqual(type(obj1.id), str)
+        model1 = BaseModel()
+        self.assertEqual(type(model1.id), str)
 
     def test_id_length(self):
         """Test that the id attribute using UUID4 is a length of 36 chars
         """
 
-        obj1 = BaseModel()
-        self.assertEqual(len(obj1.id), 36)
+        model1 = BaseModel()
+        self.assertEqual(len(model1.id), 36)
 
     def test_instance_type(self):
         """Test that the instance class is BaseModel
         """
 
-        obj1 = BaseModel()
-        self.assertIsInstance(obj1, BaseModel)
+        model1 = BaseModel()
+        self.assertIsInstance(model1, BaseModel)
 
     def test_instance_str(self):
         """Test that the string representaion of the instance is
         in the correct format
         """
 
-        obj1 = BaseModel()
-        objstr = "[BaseModel] ({}) {}".format(obj1.id, obj1.__dict__)
-        self.assertEqual(str(obj1), objstr)
+        model1 = BaseModel()
+        modelstr = "[BaseModel] ({}) {}".format(model1.id, model1.__dict__)
+        self.assertEqual(str(model1), modelstr)
 
     def test_save_method(self):
         """Test that the save method has changed the updated_at attribute
         """
 
-        obj1 = BaseModel()
-        olddate = obj1.updated_at
-        obj1.save()
-        self.assertTrue(olddate < obj1.updated_at)
+        model1 = BaseModel()
+        olddate = model1.updated_at
+        model1.save()
+        self.assertTrue(olddate < model1.updated_at)
 
     def test_create_time(self):
         """Test that the object created_at attribute is less than the current
         actual time
         """
 
-        obj1 = BaseModel()
+        model1 = BaseModel()
         date_now = datetime.now()
-        self.assertTrue(obj1.created_at < date_now)
+        self.assertTrue(model1.created_at < date_now)
 
     def test_to_dict(self):
         """Test that the dictionary returned from to_dict method has
         the correct keys and values
         """
 
-        obj1 = BaseModel()
-        dct = obj1.__dict__
-        newdct = obj1.to_dict()
+        model1 = BaseModel()
+        dct = model1.__dict__
+        newdct = model1.to_dict()
         for key in dct:
             self.assertIn(key, newdct)
 
@@ -89,3 +89,34 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(newdct["__class__"], "BaseModel")
         self.assertEqual(newdct["created_at"], dct["created_at"].isoformat())
         self.assertEqual(newdct["updated_at"], dct["updated_at"].isoformat())
+
+    def test_copy_model(self):
+        """Test the behavior of creating a model from another
+        """
+
+        model1 = BaseModel()
+        dct = model1.to_dict()
+
+        model2 = BaseModel(**dct)
+        self.assertNotEqual(model1, model2)
+        self.assertEqual(model1.id, model2.id)
+        self.assertEqual(model1.created_at, model2.created_at)
+
+    def test_copy_model_with_empty_dict(self):
+        """Test the behavior of passing an empty dictionary
+        """
+
+        dct = {}
+        model1 = BaseModel(**dct)
+        self.assertIn("id", model1.__dict__)
+
+    def test_copy_model_time_type(self):
+        """Test that the created_at and updated_at attributes are a datetime
+        objects after copying
+        """
+
+        model1 = BaseModel()
+        dct = model1.to_dict()
+        model2 = BaseModel(**dct)
+        self.assertIsInstance(model2.created_at, datetime)
+        self.assertIsInstance(model2.updated_at, datetime)
